fbf7f3b50ad9851f4c47da4500a19e4e7c53d147
Luke Reynolds <31745084+lukereynoldspi@users.noreply.github.com>
2021-09-30 22:45:44 -0700

    Add rest of code to cploration 1

--- cploration/c01/main.c
+++ cploration/c01/main.c
@@ -3,25 +3,58 @@
 /* Ex.1: Return the larger value */
 int max(int x, int y) 
 {
-   // YOUR CODE HERE
+   if (x > y)
+   {
+	   return x;
+   }
+   
+   if (y > x)
+   {
+	   return y;
+   }
+   
+   else
+	   return x;
+   
 }
 
 /* Ex.2: Add [0..100] and return the sum */
 int add100()
 {
-   // YOUR CODE HERE
+   int i = 0;
+   int num_sum = 0;
+   while(i <= 100)
+   {
+	   num_sum = num_sum + i;
+	   i++;
+   }
+   
+   return num_sum;
 }  
 
 /* Ex.3: Sum the elements in the array */
 int sum(int arr[], int n)
 {
-   // YOUR CODE HERE
+   int final_sum = 0;
+   
+	for (int i = 0; i < n; i++)
+	   final_sum += arr[i];
+
+	return final_sum;
 }  
 
 /* Ex.4: Find the largest element in the array */
 int largest(int arr[], int n)
 {
-   // YOUR CODE HERE    
+   int largest_num = 0;
+   
+	for (int i = 0; i < n; i++)
+		if (arr[i] > largest_num)
+			largest_num = arr[i];
+		
+	return largest_num;   
+	   
+	
 }
 int main() {
     // Exercise 1
